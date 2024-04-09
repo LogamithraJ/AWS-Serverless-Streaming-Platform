e69ed5c3a95cbc5b89e1df1cc66e2715e8208fd2
LogamithraJ <150684548+LogamithraJ@users.noreply.github.com>
2024-04-09 10:39:39 +0530

    Update App.js

--- App.js
+++ App.js
@@ -2,10 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <h2> Serverless Streaming Platform</h2>
       <video width="700px" height="400px" controls>
-        <source src="https://dmfgwxcsm58ep.cloudfront.net/John%20Wick%20_%20Gangsta's%20Paradise.mp4" type="video/mp4"> </source>
+        <source src="https://dmfgwxcsm58ep.cloudfront.net/John%20Wick%20_%20Gangsta's%20Paradise.mp4" type="video/mp4"></source>
+
       </video>
     </div>
   );
